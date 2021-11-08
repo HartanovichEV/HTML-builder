b4f743c7ade7735d65ecdff9cb717f43ba5b4ef2
Katiy <alexa_s_x@mail.ru>
2021-11-08 22:26:52 +0300

    feat: make task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
 
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const filesPath = path.join(__dirname, 'files-copy');
+const folderPath = path.join(__dirname, 'files');
+
+fs.mkdir(filesPath, {recursive: true}, (err) => {
+	if (err) throw err;	
+});
+
+ 	fs.readdir(filesPath, function (err, files) {
+        if (err) throw err;
+        files.forEach(function (name) {
+			const fileNew = path.join(filesPath, name);
+			fs.unlink (fileNew, err => {
+        		if (err) throw err;
+        	});
+        });
+	}); 
+	
+    fs.readdir(folderPath, function (err, files) {
+        if (err) throw err;
+        
+        files.forEach(function (name) {
+            const filePath = path.join(folderPath, name);
+            const fileNew = path.join(filesPath, name);
+            //fsPromises.unlink (fileNew);
+            fsPromises.copyFile( filePath , fileNew )
+				.then( function () {
+				//console.log( "File Copied" );
+				})
+				. catch ( function (error) {
+				console.log(error);
+			});
+   		 });
+   	});
